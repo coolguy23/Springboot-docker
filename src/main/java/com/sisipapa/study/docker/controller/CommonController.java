d0a8297b02e1de398fe8bfe5914d8734acd84707
KKH <coolguy239@naver.com>
2021-09-13 23:26:30 +0900

    version API 추가

--- src/main/java/com/sisipapa/study/docker/controller/CommonController.java
+++ src/main/java/com/sisipapa/study/docker/controller/CommonController.java
@@ -5,6 +5,13 @@ import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 public class CommonController {
 
@@ -14,6 +21,36 @@ public class CommonController {
     @GetMapping("/")
     public String port(){
         return env.getProperty("server.port");
+    }
+
+    @GetMapping("/version")
+    public String version(){
+        return "v2";
+    }
+
+    @GetMapping("/hostname")
+    public String hostname(HttpServletRequest request)  {
+        return getLocalHostName();
+    }
+
+    private static String getLocalHostName(){
+        String hostname = System.getenv("HOSTNAME");
+        if(hostname !=null) {
+            return hostname;
+        }
+
+        String lineStr ="";
+        try {
+            Process process = Runtime.getRuntime().exec("hostname");
+            BufferedReader br =new BufferedReader(new InputStreamReader(process.getInputStream()));
+            while((lineStr = br.readLine()) !=null ){
+                hostname = lineStr;
+            }
+        }catch (IOException e) {
+            e.printStackTrace();
+            hostname =null;
+        }
+        return hostname;
     }
 
 }
