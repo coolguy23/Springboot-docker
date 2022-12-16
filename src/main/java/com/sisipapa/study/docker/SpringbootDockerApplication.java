9684c9fb78446063eecaee6c6fb5f14ebe992ae1
김경훈 <74582164+sisipapa@users.noreply.github.com>
2022-12-16 21:37:21 +0900

    Update SpringbootDockerApplication.java
    fork test를 위한 주석추가

--- src/main/java/com/sisipapa/study/docker/SpringbootDockerApplication.java
+++ src/main/java/com/sisipapa/study/docker/SpringbootDockerApplication.java
@@ -3,6 +3,7 @@ package com.sisipapa.study.docker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// SpringBootApplication
 @SpringBootApplication
 public class SpringbootDockerApplication {
 
